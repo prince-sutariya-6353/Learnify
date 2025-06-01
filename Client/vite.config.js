856aea69d57b048ed6842863b6264224e20db03f
prince-sutariya-6353 <princegithub6353@gmail.com>
2025-06-01 21:57:17 +0530

    first commit

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
